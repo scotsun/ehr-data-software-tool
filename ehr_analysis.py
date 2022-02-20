c7e56612c4f25ecb349e7b6b33c579f74f12fcfb
Scott Sun <minghuisunscott@gmail.com>
2022-02-20 16:39:00 -0500

    refactor ehr_analysis using classes

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -10,6 +10,55 @@ from datetime import datetime
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
 
 
+class Lab:
+    """Lab class."""
+
+    def __init__(
+        self, pid: str, aid: str, name: str, value: float, date: datetime
+    ) -> None:
+        """Initialize."""
+        self._pid = pid
+        self._aid = aid
+        self._name = name
+        self._value = value
+        self._date = date
+
+
+class Patient:
+    """Patient class."""
+
+    def __init__(self, pid: str, gender: str, dob: datetime, race: str) -> None:
+        """Initialize."""
+        self._pid = pid
+        self._gender = gender
+        self._dob = dob
+        self._race = race
+        self._labs: list[Lab] = []
+
+    @property
+    def age(self) -> float:
+        """Calculate patient's age."""
+        now = datetime.now()
+        diff = now - self._dob
+        return diff.days / 365.25
+
+    def takes_lab(self, lab: Lab) -> None:
+        """Take lab."""
+        self._labs.append(lab)
+
+    @property
+    def age_at_first_admission(self) -> float:
+        """Get the age at 1st admission."""
+        if len(self._labs) == 0:
+            raise AttributeError("this patient has not taken any lab yet.")
+        min_labdate = self._labs[0]._date
+        for lab in self._labs:
+            if lab._aid == "1" and lab._date < min_labdate:
+                min_labdate = lab._date
+        diff = min_labdate - self._dob
+        return diff.days / 365.25
+
+
 def parse_data(filename: str) -> dict[str, list[str]]:
     """Parse data from .txt file.
 
@@ -42,33 +91,27 @@ def parse_data(filename: str) -> dict[str, list[str]]:
     return dataframe
 
 
-def num_older_than(age: float, patient_records: dict[str, list[str]]) -> int:
+def num_older_than(age: float, patients: list[Patient]) -> int:
     """Take the data and return the number of patients older than a given age.
 
     Time complexity is O(N) as a for-loop iterate through the parsed data.
 
     Parameters:
     age (float): Age of interest
-    patient_records (dict[str, list[str]]): Parsed data
+    patients (list[Patient]): List of Patient objects
 
     Returns:
     int:number of patients that fit the condition
 
     """
-    patient_dob = patient_records["PatientDateOfBirth"]
-    now = datetime.now()
     count = 0
-    for i in range(len(patient_dob)):
-        temp = datetime.strptime(patient_dob[i], DATE_FORMAT)
-        diff = now - temp
-        if diff.days / 365.25 > age:
+    for i in range(len(patients)):
+        if patients[i].age > age:
             count += 1
     return count
 
 
-def sick_patients(
-    lab: str, gt_lt: str, value: float, lab_records: dict[str, list[str]]
-) -> set[str]:
+def sick_patients(lab: str, gt_lt: str, value: float, labs: list[Lab]) -> set[str]:
     """Take the data and return a set of unique patients with the specified condition.
 
     Time complexity is O(N) as a for-loop iterate through the parsed data.
@@ -85,63 +128,15 @@ def sick_patients(
     """
     output: set[str] = set()
     if gt_lt == ">":
-        for i in range(len(lab_records["PatientID"])):
-            if (
-                lab_records["LabName"][i] == lab
-                and float(lab_records["LabValue"][i]) > value
-            ):
-                output.add(lab_records["PatientID"][i])
+        for i in range(len(labs)):
+            temp_lab = labs[i]
+            if temp_lab._name == lab and temp_lab._value > value:
+                output.add(temp_lab._pid)
     elif gt_lt == "<":
-        for i in range(len(lab_records["PatientID"])):
-            if (
-                lab_records["LabName"][i] == lab
-                and float(lab_records["LabValue"][i]) < value
-            ):
-                output.add(lab_records["PatientID"][i])
+        for i in range(len(labs)):
+            temp_lab = labs[i]
+            if temp_lab._name == lab and temp_lab._value < value:
+                output.add(temp_lab._pid)
     else:
         raise ValueError("incorrect string for gt_lt")
     return output
-
-
-def get_age_at_first_admission(
-    patient_id: str,
-    lab_records: dict[str, list[str]],
-    patient_records: dict[str, list[str]],
-) -> float:
-    """Calculate the age of a given patient at the first admission (based on the first lab date).
-
-    Time complexity is O(N) as a for-loop iterate through the parsed data twice
-    Parameter:
-    patient_id (str): Patient ID
-    lab_records (dict): Parsed data
-    Returns:
-    int:list of patient IDs
-    """
-    # get all the lab dates of the first adimission
-    patient_ids = lab_records["PatientID"]
-    admit_ids = lab_records["AdmissionID"]
-    lab_dates = lab_records["LabDateTime"]
-    nrow = len(patient_ids)
-    idx_list = [
-        i for i in range(nrow) if patient_ids[i] == patient_id and admit_ids[i] == "1"
-    ]
-    if len(idx_list) == 0:
-        raise ValueError("patient ID not found in lab records.")
-    # get date of the first lab
-    min_date = datetime.strptime(lab_dates[idx_list[0]], DATE_FORMAT)
-    for i in range(1, len(idx_list)):
-        temp = datetime.strptime(lab_dates[idx_list[i]], DATE_FORMAT)
-        if temp < min_date:
-            min_date = temp
-    # get patient dob
-    dob_str = None
-    for i in range(len(patient_records["PatientID"])):
-        if patient_records["PatientID"][i] == patient_id:
-            dob_str = patient_records["PatientDateOfBirth"][i]
-    if dob_str is None:
-        raise ValueError("patient ID not found in patient records.")
-    dob = datetime.strptime(dob_str, DATE_FORMAT)
-    # calculate the age
-    diff = min_date - dob
-    age = diff.days / 365.25
-    return age
